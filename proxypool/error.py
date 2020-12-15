91fdf327e76fac920bac6e7cdeb1d51d82ce3709
shaoyu <ishaoyu@qq.com>
2020-12-15 20:38:33 +0800

    1.优化IP池状态提醒；

--- proxypool/error.py
+++ proxypool/error.py
@@ -5,3 +5,12 @@ class PoolEmptyError(Exception):
 
     def __str__(self):
         return repr('代理池已经枯竭')
+
+
+class PoolNotMaxScoreError(Exception):
+
+    def __init__(self):
+        Exception.__init__(self)
+
+    def __str__(self):
+        return repr('代理池里的代理均不符合条件')
